21f043ec8b2226219bfaddcf24275ebfe648d97f
snowlight <it.s.aemt@gmail.com>
2023-05-20 13:55:01 +0900

    Status 만들기

---
+++ src/main/java/me/snowlight/gradestudy/GradeStudyApplication.java
@@ -0,0 +1,13 @@
+package me.snowlight.gradestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradeStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradeStudyApplication.class, args);
+    }
+
+}
