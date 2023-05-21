73d9a0017cded54b46be208b4e91f70cbdac973f
snowlight-aemt <it.s.aemt@gmail.com>
2023-05-21 15:24:15 +0900

    TargetExporter 기능 구현& 테스트

--- src/test/java/me/snowlight/gradestudy/TargetExporter.java
+++ src/test/java/me/snowlight/gradestudy/TargetExporter.java
@@ -1,8 +1,20 @@
 package me.snowlight.gradestudy;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 
 class TargetExporter {
     public void export(Path path, Targets targets) {
+        try (BufferedWriter bw = Files.newBufferedWriter(path)) {
+            for (User user : targets.getUsers()) {
+                bw.write(user.getId() + "=" + user.getGrade());
+                bw.newLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
