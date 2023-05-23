24e795c58df8efbb16d9543fdca4bafbdd7ac283
snowlight-aemt <it.s.aemt@gmail.com>
2023-05-23 21:59:17 +0900

    AdvanceApplier 패키지 분리

--- src/test/java/me/snowlight/gradestudy/AdvanceApplier.java
+++ src/test/java/me/snowlight/gradestudy/AdvanceApplier.java
@@ -1,7 +1,33 @@
 package me.snowlight.gradestudy;
 
-class AdvanceApplier {
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AdvanceApplier {
+    private JdbcTemplate jdbcTemplate;
+
+    public AdvanceApplier(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     public ApplyResult apply(Targets targets) {
-        return null;
+        Map<Integer, GradeCount> gradeCountMap = new HashMap<>();
+        for (User user : targets.getUsers()) {
+            int nextGrade = user.getGrade() + 1;
+            jdbcTemplate.update("update student set grade = ? where stu_id = ?",
+                    nextGrade,
+                    user.getId());
+
+            GradeCount gradeCount = gradeCountMap.get(nextGrade);
+            if (gradeCount == null) {
+                gradeCount = new GradeCount(nextGrade, 0);
+                gradeCountMap.put(nextGrade, gradeCount);
+            }
+            gradeCount.inc();
+        }
+
+        return new ApplyResult(gradeCountMap);
     }
 }
--- src/test/java/me/snowlight/gradestudy/AdvanceApplierTest.java
+++ src/test/java/me/snowlight/gradestudy/AdvanceApplierTest.java
@@ -59,31 +59,4 @@ public class AdvanceApplierTest {
         this.jdbcTemplate.update("insert into student values (?, ?)", id, grade);
     }
 
-    public class AdvanceApplier {
-        private JdbcTemplate jdbcTemplate;
-
-        public AdvanceApplier(JdbcTemplate jdbcTemplate) {
-            this.jdbcTemplate = jdbcTemplate;
-        }
-
-        public ApplyResult apply(Targets targets) {
-            Map<Integer, GradeCount> gradeCountMap = new HashMap<>();
-            for (User user: targets.getUsers()) {
-                int nextGrade = user.getGrade() + 1;
-                jdbcTemplate.update("update student set grade = ? where stu_id = ?",
-                                        nextGrade,
-                                        user.getId());
-
-                GradeCount gradeCount = gradeCountMap.get(nextGrade);
-                if (gradeCount == null) {
-                    gradeCount = new GradeCount(nextGrade, 0);
-                     gradeCountMap.put(nextGrade, gradeCount);
-                }
-                gradeCount.inc();
-            }
-
-            return new ApplyResult(gradeCountMap);
-        }
-    }
-
 }
